cd07676528e6a6c2abecc8cb4e27066f3b58759f
Aditi Rana <2006arana@gmail.com>
2026-05-13 09:17:50 +0000

    Updated files

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
+import { testBackend } from "../../api/authApi";
+
 const Dashboard = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await testBackend();
+
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Dashboard</h1>
