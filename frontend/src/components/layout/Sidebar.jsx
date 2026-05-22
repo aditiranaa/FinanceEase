96af333589059ec1df1028445b0c7659b4bf2920
Aditi Rana <2006arana@gmail.com>
2026-05-22 14:53:15 +0000

    feat: integrate Tailwind CSS into frontend

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -1,22 +1,18 @@
 const Sidebar = () => {
   return (
-    <div
-      style={{
-        width: "220px",
-        height: "100vh",
-        background: "#111827",
-        color: "white",
-        padding: "20px",
-      }}
-    >
-      <h2>FinanceEase</h2>
+    <div className="w-64 h-screen bg-gray-900 text-white p-5">
 
-      <hr />
+      <h2 className="text-2xl font-bold mb-5">
+        FinanceEase
+      </h2>
 
-      <p>Dashboard</p>
-      <p>Transactions</p>
-      <p>Budgets</p>
-      <p>Goals</p>
+      <div className="space-y-3">
+        <p>Dashboard</p>
+        <p>Transactions</p>
+        <p>Budgets</p>
+        <p>Goals</p>
+      </div>
+
     </div>
   );
 };
