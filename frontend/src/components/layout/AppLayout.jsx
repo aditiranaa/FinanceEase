7a21ee50bdb0883f9b33839ff7e82a5db9f2a8c6
Aditi Rana <2006arana@gmail.com>
2026-07-12 18:12:00 +0000

    feat(layout): add reusable application layout

---
+++ frontend/src/components/layout/AppLayout.jsx
@@ -0,0 +1,26 @@
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+export default function AppLayout({ children }) {
+  return (
+    <div className="flex min-h-screen bg-gray-100 dark:bg-gray-950">
+
+      <Sidebar />
+
+      <main className="flex-1 overflow-x-hidden">
+
+        <div className="p-6">
+
+          <Navbar />
+
+          <div className="mt-8">
+            {children}
+          </div>
+
+        </div>
+
+      </main>
+
+    </div>
+  );
+}
